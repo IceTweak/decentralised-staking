8dbcd0531f400a1cae4f43d9cc93125132e7f7ff
Роман Савощик <tweakice@gmail.com>
2023-03-10 16:37:48 +0600

    change solidity version

--- packages/hardhat-ts/hardhat.config.ts
+++ packages/hardhat-ts/hardhat.config.ts
@@ -121,7 +121,7 @@ const config: HardhatUserConfig = {
   solidity: {
     compilers: [
       {
-        version: '0.8.6',
+        version: '0.8.17',
         settings: {
           optimizer: {
             enabled: true,
